7fbca1752712147485d2ee9b115d51039081c8b6
RicharFl <ricardo_6_11@hotmail.com>
2021-05-07 21:23:24 -0500

    Practica 1 Estructura de Datos33

--- Estructura de Datos/Practica1_Pilas.c
+++ Estructura de Datos/Practica1_Pilas.c
@@ -6,62 +6,58 @@ typedef struct _NODO
     int dato;          /* Este es dato que se encuentra en mi nodo*/
     struct _NODO *sig; /*Este es el apuntador con el que estaremos uniendo nodos */
 
-}nodo;
+} nodo;
 
 /* Esta funcion Reservar memoria para ese nodo  1   int  =5555555555555 */
-nodo * reservar(int d)
+nodo *reservar(int d)
 {
-   nodo *tamano =NULL;
-   tamano=(nodo *)malloc (sizeof(int));
+    nodo *tamano = NULL;
+    tamano = (nodo *)malloc(sizeof(int));
 
-   if (tamano!=NULL)
-   {
-       tamano-> dato =d;
-       tamano ->sig =NULL;
+    if (tamano != NULL)
+    {
+        tamano->dato = d;
+        tamano->sig = NULL;
         return tamano;
-   }
-   else 
-   {
-       return NULL;
-
-   }
-
+    }
+    else
+    {
+        return NULL;
+    }
 }
 
-
-nodo * Alta (int dato, nodo * cima)
+nodo *Alta(int dato, nodo *cima)
 {
- 
- 
-  nodo * aux=NULL;
-  aux = reservar(dato);
-  if (cima != NULL)
-  {
-      aux ->sig=cima;
-      return aux;
-  }
-  else {
-      return aux;
-  }
 
+    nodo *aux = NULL;
+    aux = reservar(dato);
+    if (cima != NULL)
+    {
+        aux->sig = cima;
+        return aux;
+    }
+    else
+    {
+        return aux;
+    }
 }
 
-void mostrar ( nodo *cima)
+void mostrar(nodo *cima)
 {
-    int i =0;
-    if ( cima ==NULL)
+    int i = 0;
+    if (cima == NULL)
     {
         printf("La  Pila esta Vacia");
     }
-    else{
+    else
+    {
         while (cima != NULL)
         {
-         printf ("ELEMNTO [%d] : %d \n",i,cima->dato);
-         
-         cima =cima ->sig;
-         i++;
+            printf("ELEMNTO [%d] : %d \n", i, cima->dato);
+
+            cima = cima->sig;
+            i++;
         }
-        
     }
 }
 
@@ -69,9 +65,9 @@ int main(void)
 
 {
 
-    nodo * Pila = NULL;
+    nodo *Pila = NULL;
     char menu = 's';
-    int ops = 0,dato=0;
+    int ops = 0, dato = 0;
     while (menu == 's')
     {
         printf("\n BIENVENIDO ESCOJE ALGUNA DE NUESTRA OPCIONES\n \n");
@@ -82,27 +78,24 @@ int main(void)
         switch (ops)
         {
         case 1:
-        system("cls");
-        printf("Ingresa tu dato\n ");
-        scanf ("%d",&dato);
-            Pila=Alta(dato,Pila);
-        printf("El dato a sido ingresado Correctamente \n \n");
-        system ("pause");
+            system("cls");
+            printf("Ingresa tu dato\n ");
+            scanf("%d", &dato);
+            Pila = Alta(dato, Pila);
+            printf("El dato a sido ingresado Correctamente \n \n");
+            system("pause");
 
             break;
         case 2:
             break;
         case 3:
-        system("cls");
-        mostrar(Pila);
-         system ("pause");
+            system("cls");
+            mostrar(Pila);
+            system("pause");
             break;
-        
 
         default:
             break;
         }
     }
 }
-
-
