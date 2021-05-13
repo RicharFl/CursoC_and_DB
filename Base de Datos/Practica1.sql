5c6b3ec040f21109d70e70674135d9e874ce070d
RicharFl <ricardo_6_11@hotmail.com>
2021-05-12 21:27:29 -0500

    Nueva Ava

--- Base de Datos/Practica1.sql
+++ Base de Datos/Practica1.sql
@@ -25,6 +25,42 @@ insert into Alumnos (NombreAlumno,ApellidoAlumno,BoletaAlumno,Telefono,edad) val
                     ("ALEXIS","RAMIRE3",2016301926,55193657559,17),
                     ("CARLOS4","RAMIRE4",2016301927,55193657559,25),
                     ("CARLOS4","RAMIRE4",2016301929,55193657559,25);
+                    
+                  select distinct edad from Alumnos;
+                  
+                  select *from Alumnos where NombreAlumno="EDGAR";
+                  
+                  /*Desarrolla una consulta que me devuelva los alumnos que sean mayores de 20 años.
+                  */
+                  select *from Alumnos where edad>=20;
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                  
+                    
+create table Materias(
+IdMateria varchar(10)not null,
+NombreMateria varchar(15)not null,
+primary key(IdMateria)
+);
+
+select *from Materias;
+insert into Materias(IdMateria,NombreMateria) values
+            ("HF-402025","CALCULO"),
+            ("HF-402026","INGLES"),
+            ("HF-402027","FISICA"),
+            ("HF-402028","PROGRAMACION"),
+            ("HF-402029","BASE DE DATOS");
 	
   select * from Alumnos;  /* Consulta  de la tabla Alumno */
  
@@ -39,4 +75,76 @@ select distinct  edad,NombreAlumno from Alumnos;
 select *from Alumnos Where edad =18;
 select *from Alumnos where edad between 16  and 21;
                     
-                    
+              select * from Materias;  
+/*GENERA UNA CONSULTA QUE ME RETORNE  SOLO LA MATERIA DE INGLES  */    
+select * from Materias where NombreMateria="INGLES";
+/*GENERA UNA CONSULTA QUE ME RETORNE  EL REGISTRO DE DEL ALUUMNO CARLOS2    */
+Select * from Alumnos where NombreAlumno ="CARLOS2";
+
+/*GENERA UNA CONSULTA QUE ME RETORNE  EL REGISTRO DE DEL ALUUMNO 2016301912 solo me muestre Nombre y Bolete*/
+select NombreAlumno , BoletaAlumno FROM Alumnos where BoletaAlumno =2016301912;
+   
+/*GENERA UNA CONSULTA QUE ME RETORNE  EL REGISTRO DE DEL ALUUMNO 2016301912 y 2016301915 solo me muestre Nombre y Bolete*/
+select NombreAlumno ,BoletaAlumno from Alumnos where BoletaAlumno=2016301912 or BoletaAlumno=2016301915 ;
+
+
+
+create table PROFESORES(
+    IdProfesor varchar(9)not null,
+    Nombre varchar(15)not null,
+    Apellido varchar(15)not null,
+    primary key(IdProfesor)  
+    );
+            
+ insert into PROFESORES(IdProfesor,Nombre,Apellido) values
+					("CPF-4020","RICARDO","RAMIREZ"),
+                    ("CPF-4021","JOSS","SANCHEZ"),
+                    ("CPF-4022","CARMEN","QUIJANO"),
+                    ("CPF-4023","ALEJANDRA","PORFIERO"),
+                    ("CPF-4024","SANCHEZ","DIAZ");
+
+
+
+
+
+
+/*GENERA UNA CONSULTA QUE ME RETORNE  SOLO LA MATERIA DE INGLES  */ 
+   select *from Materias where NombreMateria="INGLES";
+/*GENERA UNA CONSULTA QUE ME RETORNE  EL REGISTRO DE DEL ALUUMNO CARLOS2    */
+select *from Alumnos where NombreAlumno="CARLOS2";
+/*GENERA UNA CONSULTA QUE ME RETORNE  EL REGISTRO DE DEL ALUUMNO 2016301912 solo me muestre Nombre y Bolete*/
+select NombreAlumno,BoletaAlumno from Alumnos where BoletaAlumno=2016301912;
+/*GENERA UNA CONSULTA QUE ME RETORNE  EL REGISTRO DE DEL ALUUMNO 2016301912 y
+ 2016301915 solo me muestre Nombre y Bolete*/
+ SELECT Alumnos.NombreAlumno, Alumnos.BoletaAlumno from Alumnos 
+ where BoletaAlumno=2016301912 or BoletaAlumno=2016301913 ;
+select *from Alumnos;
+ /*Genera una consulta que Retorne la materia con id -> HF-402025 */
+ select  *from Materias where IdMateria="HF-402025";
+ /* Genera una consulta que Retorne los alumnos que sean mayores a 21 y excluya alos de 25 
+*/ 
+select *from Alumnos where edad >21 and edad <> 25;
+select *from Alumnos;
+
+
+select *from Alumnos where edad between 20  and 22;
+
+
+
+select *from Alumnos;
+Select *from Materias;
+Select *from Profesores;
+
+create table Horario(
+	IdHorio int (10),
+    BoletaIdHorario int (10) not null,
+    IdMateriaHorario varchar(10)not null,
+    IdProfesorHorario varchar (9) not null,
+    primary key (IdHorio),
+    foreign key (BoletaIdHorario) references Alumnos (BoletaAlumno),
+    foreign key  (IdMateriaHorario) references Materias (IdMateria),
+    foreign key (IdProfesorHorario) references Profesores (IdProfesor)
+);
+insert into horario (IdHorio,BoletaIdHorario,IdMateriaHorario,IdProfesorHorario) values 
+(1,2016301921,"HF-402028","CPF-4021");
+select *from Horario;
